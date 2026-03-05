0c3861096461a323f6912a3df06ca561462d9abe
yeray-752 <yeraycarrioncero21@gmail.com>
2026-03-05 18:13:47 +0000

    first commit

---
+++ routes/api/bicicletas.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let router = express.Router();
+let BicicletaControllerAPI = require("../../controllers/BicicletaControllerAPI");
+
+
+router.get("/", BicicletaControllerAPI.bicicleta_list);
+router.post("/create", BicicletaControllerAPI.bicicleta_create);
+router.delete("/delete", BicicletaControllerAPI.bicicleta_delete);
+router.put("/update", BicicletaControllerAPI.bicicleta_update);
+
+module.exports = router;
